d70f85ff6d2be8fddf84aaddf0cd714f3a3a6a4f
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-27 14:54:38 +0200

    Add titol and type

--- src/components/YuGiCard.vue
+++ src/components/YuGiCard.vue
@@ -9,9 +9,10 @@ export default {
 
 <template>
     <div class="card">
-        <pre> {{ this.card }}</pre>
+
         <img :src="this.card.card_images[0].image_url" alt="">
         <h2>{{ this.card.name }}</h2>
+        <p>{{ this.card.archetype }}</p>
     </div>
 </template>
 
